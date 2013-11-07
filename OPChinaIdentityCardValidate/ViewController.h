744a74b750653fa0d4ceaa5f7e15c6d9dc46f068
Sean-Wang <exiang.bee@gmail.com>
2013-11-07 15:23:19 +0800

    Initial Commit

---
+++ OPChinaIdentityCardValidate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OPChinaIdentityCardValidate
+//
+//  Created by Sean on 11/7/13.
+//  Copyright (c) 2013 opomelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
